5f78d96cadb143090711229100bb87bb1652e458
md-naderi <md.naderi079@gmail.com>
2021-08-31 12:15:09 +0430

    Project 2 - GroceryBud Updated
    Item Edit,Delete,Clear All & Local Storage Added to project

--- Phase-1(Html-Css-Js)/GroceryBud/function.js
+++ Phase-1(Html-Css-Js)/GroceryBud/function.js
@@ -1,20 +1,62 @@
 var selctedItem;
+var size = 0;
+	function funcLoad()
+	{
+		if(localStorage.getItem("size") == null)
+		{
+			localStorage.setItem("size", 0);
+		}
+		else
+		{
+			size = eval(localStorage.getItem("size"));
+		}
+
+		for(var i=1 ; i<=size ; i++)
+		{
+			var n = 'item'+i;
+			var itemName = localStorage.getItem(n);
+
+			var mytr = document.createElement('tr');
+			mytr.innerHTML = "<td class='item'>"+itemName+"</td> <td class='icon' onclick='funcedit(this)'><img src='edit-icon.png'></td><td class='icon' onclick='funcDelete(this)'> <img src='delete-icon.png'></td>";
+			var t = document.getElementById('mytable');
+			t.appendChild(mytr);
+		}
+	}
+
 	function funcSubmit()
 	{
+		ShowDlg('http://google.com');
 		if(submitButton.value =='Submit')
 		{
 			var text = document.getElementById('txtBox').value;
 			var mytr = document.createElement('tr');
-			mytr.innerHTML = "<td class='item'>"+text+"</td> <td class='icon' onclick='funcedit(this)'><img src='edit-icon.png'></td><td class='icon'> <img src='delete-icon.png'></td>";
+			mytr.innerHTML = "<td class='item'>"+text+"</td> <td class='icon' onclick='funcedit(this)'><img src='edit-icon.png'></td><td class='icon' onclick='funcDelete(this)'> <img src='delete-icon.png'></td>";
 			var t = document.getElementById('mytable');
 			t.appendChild(mytr);
 			document.getElementById('txtBox').value = "";
+
+			size = size + 1;
+			localStorage.setItem("size", size);
+			var itemName = 'item'+size;
+			localStorage.setItem( itemName, text);
+
 		}
-		else
+		else	//Edit
 		{
+			var text = document.getElementById('txtBox').value;
 			selctedItem.innerHTML = document.getElementById('txtBox').value;
 			document.getElementById('txtBox').value = "";
 			submitButton.value ='Submit';
+
+			var i=0;
+			for( ; i<=size ; i++)
+			{
+				if(document.getElementById('mytable').getElementsByTagName('tr')[i] == selctedItem.parentNode)
+					break;
+			}
+			i = i+1;
+			var itemName = 'item'+i;
+			localStorage.setItem( itemName, text);
 		}
 	}
 
@@ -25,3 +67,35 @@ var selctedItem;
 		selctedItem = t.previousSibling.previousSibling;
 	}
 	
+	function funcDelete(s)
+	{
+		var i=0;
+		for( ; i<=size ; i++)
+		{
+			if(document.getElementById('mytable').getElementsByTagName('tr')[i] == s.parentNode)
+				break;
+		}
+		document.getElementById('mytable').removeChild(s.parentNode);
+		for( i=i+1; i<=size-1 ; i++)
+		{
+			var itemName = 'item'+i;
+			var itemName2 = 'item'+(i+1);
+			localStorage.setItem( itemName, localStorage.getItem(itemName2));
+		}
+		localStorage.removeItem('item'+size);
+		size = size-1;
+		localStorage.setItem("size", size);
+	}
+
+	function funcClearItems()
+	{
+		var i=0;
+		for( ; i<=size ; i++)
+		{
+			var itemName = 'item'+(i+1);
+			localStorage.removeItem(itemName);
+		}
+		document.getElementById('mytable').innerHTML = " ";
+		size = 0;
+		localStorage.setItem('size' , 0);
+	}
